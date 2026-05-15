4c57778cc18f10aa34a58ba6accfbc33f1d89806
Prem Mohan <premmohan966@gmail.com>
2026-05-15 11:08:27 +0530

    Update Prem_Mohan_Resume.py

--- Prem_Mohan_Resume.py
+++ Prem_Mohan_Resume.py
@@ -482,10 +482,11 @@ html_content = """
     <div style="margin-bottom: 20px;">
     <strong>🏢 Present Position</strong><br>
     Working as Data Analyst & Data Management Assistant with Personal Assistance Experience<br>
-         🇮🇳 Government of India<br>
+        🇮🇳 Government of India<br>
 📍 National Instructional Media Institute (NIMI), Chennai<br>
 🏛️ Directorate General of Training (DGT)<br>
 🚀 Ministry of Skill Development & Entrepreneurship (MSDE)
+
 
 </div>
 
